81048b455ce7912c3eb715d7b0b084dcd8af6bc0
Onyekachi <onye@smmaven.com>
2016-02-13 15:50:09 -0800

    Position

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+def position_taken?(board,pos)
+	if board[pos] == " "
+		return false
+	elsif board[pos] == ""
+		return false
+	elsif board[pos] == nil
+		return false
+	else
+		return true
+	end
+		
+end
