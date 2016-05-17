f68d8c02d0cdbc2dad77efbd53fa330c7bd69a3f
Adam Cooper <amcooper@gmail.com>
2016-05-17 00:46:58 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board,position)
+  content = board[position]
+  if content == "" || content == " " || content == nil 
+    return false
+  elsif content == "X" || content == "O"
+    return true
+  else
+    return nil
+  end
+end
