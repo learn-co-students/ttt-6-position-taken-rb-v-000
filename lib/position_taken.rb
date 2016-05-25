eed993eff7700e6c35e764d3d14631cfd1b7814a
reichardn <reichardn@gmail.com>
2016-05-25 12:45:51 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  i = board[index]
+  if i == " " || !(!!i) || i ==""
+    return false
+  else
+    return true
+  end
+end
