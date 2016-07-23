2564f622e458fe1f1a184762a3455655e36b9f9c
Leonard Bernardo <leonard.bernardo@gmail.com>
2016-07-23 03:48:47 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+if
+board[index] == " " ||
+board[index] == "" ||
+board[index] == nil
+false
+else
+board[index]  == "X" ||
+board[index]  == "O"
+true
+end
+end
+
+
+###you have to spell it out every time like board == " " || board == "" || board == nilyhere
+### methods that end with  a ?  must return  TRUE or false
+#### Reminder: Remember that to check for equality, we use the == operator, not the = operator. The = operator is the assignment operator, it sets a variable equal to a value. The == operator is the equality operator. It returns true if the value on the left side of the operator is the same as the value on the right side of the operator. Otherwise it returns false. To get this test passing, you could also use the !=, or "not equal" operator.
+### You'll be able to use if/else or simple boolean operators like &&, ||, or !=, or '=='.
+### Once you implement this first check, you'll see the next error about an edge case, ""
