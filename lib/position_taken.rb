c1084810d8a3b472f73ec6ef08aa28e087d7481d
Kenneth Earley <devinearley@yahoo.com>
2016-08-09 20:36:01 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == " " || "" || nil
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
     return false
   end
 end
