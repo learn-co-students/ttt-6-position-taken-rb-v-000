3cf6fe4248e72c3c27212594b5d8c4bf1c533bd4
Bianca Laurendine <bnlauren@gmail.com>
2016-10-06 16:05:50 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,3 +5,12 @@ def position_taken?(board, index)
      true
    end
  end
+
+ def valid_move?(board, index)
+    if index.between?(0, 8) && position_taken?(board, index) == false
+      true
+    else
+      false
+
+    end
+  end
