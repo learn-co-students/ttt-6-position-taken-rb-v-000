51ea69f8a0c81aad18310169b60a1530491e9ff1
IDAYAT OSHOBAJO <idayat.oshobajo@gmail.com>
2016-11-06 14:39:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index[0]] == "X"
+    true
+  elsif board[index[0]] == " "
+    false
+  elsif board[index [0]] == ""
+    false
+  elsif board[index[0]] == nil
+    false
+      end
+end
