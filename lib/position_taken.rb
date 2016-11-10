e6c8b25783feae8e55e24b6d3c6abd4a33bcbdac
Masha Podgorbunskikh <mashapie23@yahoo.com>
2016-11-10 21:47:57 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
-# code your #position_taken? method here!
+def position_taken?(board, position)
+
+def display_board(board)
+  board = [" "," "," "," "," "," "," "," "," "]
+	puts " #{board[0]} | #{board[1]} | #{board[2]} "
+	puts "-----------"
+	puts " #{board[3]} | #{board[4]} | #{board[5]} "
+	puts "-----------"
+	puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+  if board[position] == "X" || board[position] == "O"
+    return true
+  else
+    return false
+  end
+end# code your #position_taken? method here!
