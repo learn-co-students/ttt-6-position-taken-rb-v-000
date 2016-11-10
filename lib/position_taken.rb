2ea505e85ffa7849fde2ae05061a3f45d5e784dc
Jennifer Green <greenjenniferb@gmail.com>
2016-11-10 21:22:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,20 @@
 # code your #position_taken? method here!
+#def position_taken?(board, index)
+#  if board["X".to_i || "O".to_i] == board["X".to_i || "O".to_i]
+  #elsif board[" ".to_i || "".to_i] !=
+  #else board[nil] !=
+  #end
+#end
+# true = spot taken
+# false = spot free
+#def position_taken?(board, index)
+  #if board[" ".to_i] == "X" || "O"
+
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O" # condition
+    true #here we need it to do something "if above condition is true then it will say this"
+  else
+    false
+  end
+end
