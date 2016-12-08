e5546bffe8e3311199f7e22672a37fe7a915f91c
Elias Ventura <eliasventura23@gmail.com>
2016-12-08 00:49:47 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,8 @@
 # code your #position_taken? method here!
-require 'pry'
 def position_taken?(board, index)
-
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  elsif
-    board[index] == "X" || "O"
+  elsif board[index] == "X" || "O"
     true
   end
 end
