6e325e821237ab1bbbce621baa6aef34ec76b4be
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-01-15 11:35:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  pos = board[index]
+  return pos == "X" || pos == "O"
+end
+
+#You can also do this as follows
+#def position_taken?(board, index)
+#  suggested_position = board[index]
+#  return ["X", "O"].include? suggested_position
+#end
