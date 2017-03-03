da6ebfb166a7ad32defc70fc4a64b31122e8212d
Beth Turner <englishturner@gmail.com>
2017-03-03 19:20:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+#trying to see if there is already an X or O in the space
+#we are validating user input
+
+def position_taken?(board,index)
+  board[index] == "X" || board[index] == "O"
+end
