845ef03650c37fa481da75e270889634a78cfad0
nathalie garcia <nathalie.scruz@gmail.com>
2017-03-25 00:22:38 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,8 +4,10 @@ def position_taken? (board, index)
   index .to_i - 1
     if board[index] == "X" || board[index] == "O"
       return true 
-    else board[index] == " " || board[index] == "" || board[index] == nil
+    elsif board[index] == " " || board[index] == "" || board[index] == nil
       return false 
+    else 
+      return false
   end
 
 end
