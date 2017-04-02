9706ccbadfa44ec06aa7ce9848a8bb5c0a8f35b9
Alex Jelly <alexjelly8@gmail.com>
2017-04-02 22:40:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
 # code your #position_taken? method here!
+# methods that are true or false have a question mark "?" at the end of the naming convention
+def position_taken?(board, index)
+    if board[index] == " "
+      false
+    elsif board[index] == ""
+      false
+    elsif board[index] == nil
+      false
+    elsif board[index] == "X"
+      true
+    elsif board[index] == "O"
+      true
+    else
+      
+    end
+end
