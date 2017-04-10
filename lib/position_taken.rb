e4f06ef455047a4d2d3eb3a0dcb685c41a6b27b4
Brian Zoebisch <zoebisch@gmail.com>
2017-04-10 18:34:58 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " "
+    return false
+  elsif board[index] == nil
+    return false
+  elsif board[index].upcase == "X" || board[index].upcase == "O"
+    return true
+    end
+end
