331607e005cb2cfc40abbd13eca56525d3ae4a0e
ctab33 <ctab33@me.com>
2017-05-02 10:40:34 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  board.include?("")
+  !board.include?(" ")
+  board.include?("X" || "O")
+  #if (board.each == X || board.each == O)
+    #return false
+ #if board[index] == X
+   #return true
+end
+
+
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#board.include?("") #=> false
+#board.include?(" ") #=> true
