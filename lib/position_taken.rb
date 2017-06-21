577dd77378403a01c1e3eb0f5af416cb720ae67d
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2017-06-21 23:55:00 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(array,int)
+  if array[int] == " " || array[int] == "" || array[int] == nil
+    return false
+  else
+    return true
+  end
+end
