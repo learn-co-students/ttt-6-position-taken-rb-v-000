ec4888870a4103d9eb15a1841d5cb4ce69d4e6f0
Briana Gross <blgross@d.umn.edu>
2017-07-11 10:25:02 -0500

    Update position_taken.rb

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == " "
-    false
-  elsif board[index] == ""
-    false
-  elsif board[index] == nil
-    false
-  elsif board[index] == "X"
+  if board[index] == "O" || board[index] == "X"
     true
-  elsif board[index] == "O"
-    true
+  else
+    false
   end
 end
