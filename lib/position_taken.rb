545854b7216ea6424b6f2be84d0f84fd2012eafb
Cat Norris <cannenorris@yahoo.com>
2017-07-17 22:35:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+
+  if (board[index]==" ")=="false" || (board[index]=="")=="false"
+ puts "That position is already taken"
+
+  end
+end
