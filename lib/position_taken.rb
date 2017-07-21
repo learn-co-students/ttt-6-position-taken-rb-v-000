c758f3efeb5c2468853ad8f88b8087ff8325208c
Valencia Caldwell <vkcaldwell@gmail.com>
2017-07-21 17:07:56 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,23 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+end
+def index_number(board, index)
+  gets.strip= index_number
+end
+def array(board)
+end
+def array[index]
+  if array[index] == ""
+    return "false"
+  end
+end
+# def position_taken?(board, index)
+#   gets.strip = Integer
+# end
+# def array[index]
+# end
+# def position_taken?(board, index)
+#   if array[index_number] == ""|| array[index_number]== " " || array[index_number]== "nil"
+#     return "false"
+#   end
+# end
+# end# end# code your #position_taken? method here!
