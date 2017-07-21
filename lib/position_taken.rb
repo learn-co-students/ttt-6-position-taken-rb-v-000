40ba19b4367fb2983e3dff5b890c36d0d56d7aa3
Valencia Caldwell <vkcaldwell@gmail.com>
2017-07-21 17:29:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,23 +1,4 @@
 def position_taken?(board, index)
+  (board[index] == "")? true:false
+  (board[index])== "X" "O")? true
 end
-def index_number(board, index)
-  gets.strip= index_number
-end
-def array(board)
-end
-def array[index]
-  if array[index] == ""
-    return "false"
-  end
-end
-# def position_taken?(board, index)
-#   gets.strip = Integer
-# end
-# def array[index]
-# end
-# def position_taken?(board, index)
-#   if array[index_number] == ""|| array[index_number]== " " || array[index_number]== "nil"
-#     return "false"
-#   end
-# end
-# end# end# code your #position_taken? method here!
