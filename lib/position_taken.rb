0c88835b62c3a39ef26872e82e29e65acced532e
Hieu <github email address>
2017-08-02 20:39:38 +0430

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+    if board == " "
+        return false
+    else board.include?("X") || board.include?("O")
+    end
+end
