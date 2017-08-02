e3e68dc61fa21c9b539f4952dc7e5f758338abc3
Kevin Kim <kevikim33@gmail.com>
2017-08-02 21:19:38 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+
+def position_taken?(board, index)
+  if (board[index] == nil || board[index].strip == "")
+    false
+  elsif (board[index].strip == "X" || board[index].strip == "O")
+    true
+  end
+end
