f682281bddd0db5f8ce3df1ecb25c3bd921d9c49
Augusto Granitto <augustogranitto@gmail.com>
2017-08-13 15:25:42 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,6 @@
 # code your #position_taken? method here!
+def position_taken?(board,position)
+  return false if [" ", "", nil].include?(board[position])
+  return true if ["X", "O"].include?(board[position])
+  raise "#{board[position]} is not a valid move"
+end
