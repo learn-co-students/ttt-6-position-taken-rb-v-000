c22f2430bb12c06d3f6986e3cc9393f3e83ef356
Hunter Kleindl <hkleindl@gmail.com>
2017-08-17 22:39:24 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
+#def position_taken?(board, index)
+#  board[index] != " " && board[index] != "" && board[index] != nil
+#end
+
 def position_taken?(board, index)
-  board[index] != " " && board[index] != "" && board[index] != nil
+  board[index] == "X" || board[index] == "O"
 end
