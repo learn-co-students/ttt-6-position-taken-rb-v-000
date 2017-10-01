7c6f21d276fe70ffdbcf651343999a7623849491
Joshua Needham <joshuabneedham@gmail.com>
2017-10-01 01:45:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  #Check for an empty board is equal to " "
+  if board[index] == " "
+    return false
+  end
+  if board[index] == ""
+    return false
+  end
+  if board[index] == nil
+    return false
+  end
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
