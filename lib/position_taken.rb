ca9d2db8306a0b7e61e318010f1cccb9aea44cc6
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-06 00:41:23 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " || board[index] = ""
+  # check the value of the board at the given index
+
+  # check the board at the index being passed through
+  # check to see if the board[index] = " "
+end
