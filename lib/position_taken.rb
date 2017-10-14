09ccbb976a7839b0a264c8a1ced91d9c69045453
Connor Gallopo <connor.gallopo@me.com>
2017-10-14 14:00:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    # you got hung up here because you didnt put board[index] for each check. you only checked for " ". not "" or nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+
+  end
+
+end
