d87d836337d94ca0ca0c7b388f15c4de83a92fcc
Amanda Ackerman <ackerm44@gmail.com>
2017-10-16 17:26:47 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(array, index_number)
+  if array[index_number] == "X" || array[index_number] == "O"
+    return true
+  else
+    return false
+  end
+end
