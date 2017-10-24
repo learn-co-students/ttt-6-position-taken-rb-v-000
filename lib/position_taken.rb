9186995fd6783b86da43954cfcd185deb2a712f9
mstmari <michael.landrum.st.marie@gmail.com>
2017-10-24 15:52:18 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  index.to_i - 1
+  if board[index] == "X"
+    true
+  elsif board[index] == "O"
+    true
+  elsif board[index] == " " or ""
+    false
+  end
+end
