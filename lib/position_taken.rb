637c72c2720516dc991045d8e16047154f4a5f2c
Chris Kihlstrom <ckihlst@gmail.com>
2017-10-26 23:35:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board,index)
   if index == " " || index == ""
-  elsif index == "X" && index == "O"
+  elsif index == "X" || index == "O"
   elsif index != nil
   else
     true
