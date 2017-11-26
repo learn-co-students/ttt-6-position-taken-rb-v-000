7630e1f1fd867132892e840279e1c55fa7577b1d
Kristen Leach <kristenleach24@gmail.com>
2017-11-26 19:34:29 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(array, index_number)
-  if  (array[index_number] == "") || (array[index_number == " "])
+  if  array[index_number] == "" || array[index_number == " "]
     return false
-  elsif (array[index_number] != "") || (array[index_number] != " ")
+  elsif array[index_number] != "" || array[index_number] != " "
     return true
   end
 end 
