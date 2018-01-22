10323462db27b620425fe64c52ab1afabb62dd06
Calista Nasser <nasser.calista@gmail.com>
2018-01-22 06:21:06 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  index_number = index.to_i
+  taken = nil
+  
+  if board[index_number] != 'X' && board[index_number] != 'O'
+    taken = false
+  else taken = true
+  end
+end
