61bf0a2ceeb6e2254b374959e82f5133ed9d2077
Stephen Harrington <stephenharrington04@yahoo.com>
2018-01-30 19:26:19 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  else board[index] == "X" || board[index] == "O"
+  else
     true
   end
 end
