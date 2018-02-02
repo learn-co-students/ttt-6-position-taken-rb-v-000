3e2eb150dbf0017652bb1f380ccdf2181ad78c99
Daniel Hope <upinhope@gmail.com>
2018-02-02 20:33:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+# method to check if the spot on board is taken or empty
+
+def position_taken?(array, index)
+  if (array[index] == "X") || (array[index] == "O")
+    return true
+  elsif (array[index] != " ") || (array[index] != "") || (array[index] != nil)
+    return false
+  end
+end
