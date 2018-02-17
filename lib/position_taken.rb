b3c693cd730354863c678ffb6700a5d116864f30
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-02-17 19:00:31 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  else board[index] != "" || board[index] != " "
+    true
+end
+end
