361fad5c4918f19f839d87a778a961c6cdd28c4d
Kitty Davies <kittyfdavies@gmail.com>
2018-02-27 20:47:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(array, index_number)
-  if array[index] == " "
+  if array[index_number] == " "
     false
-  elsif array[index] == ""
+  elsif array[index_number] == ""
     false
-  elsif array[index] == nil
+  elsif array[index_number] == nil
     false
   else
     true
