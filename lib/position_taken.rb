e3d9989fb14a01c8e6b0852ab2b5082644f69e7b
Patrick McCombs <pa.mccombs1@gmail.com>
2018-03-20 19:07:55 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    then false
+     elsif board[index] == "X" || "O"
+    then true
+  else
+    true
+  end
+end
+  
