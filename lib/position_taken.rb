79143395c6d6c938b63b3f3794ee6e3e7539f88c
Anuradha Khambete <anukhambete@gmail.com>
2018-03-28 20:37:40 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+    x=nil
+    if board[index] = " " || "" || nil  && board[index]!= "X" || "O"
+    x=false
+      else
+      x=true
+  end
+  x
+end
