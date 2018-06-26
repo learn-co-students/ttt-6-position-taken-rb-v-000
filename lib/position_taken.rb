8612339a3cb4cf187d23d6392710336a860f17c9
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-26 11:07:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@
 
 def position_taken?(board, index)
 
-  if board[index] == " " || board[index] == "" || board[index]==nil
+  if board[index] == " " || board[index] == "" || board[index]== nil
     FALSE
   else
     TRUE
