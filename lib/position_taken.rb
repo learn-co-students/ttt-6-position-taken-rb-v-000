4d33c9f911dd74f46b68dfd738ba69e355fe6c31
Tammy Metz <tammy@tammymetz.com>
2018-06-27 14:09:42 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  return_value = false
+  if board[index] == "X" || board[index] == "O"
+    return_value = true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    return_value = false
+  else
+    return_value = false
+  end
+  return return_value
+end
