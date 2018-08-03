e545883cadeb77adef36023cbb2a5b0010f56fd9
Laura Darlak <laura@polyporedesign.com>
2018-08-03 17:17:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,19 @@
 # code your #position_taken? method here!
 
 def position_taken? (board, index)
-  board[index] != " " && board[index] !=  "" && board[index] != nil
+
+  # basic solution:
+
+ # taken = nil
+ # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+ #   taken = false
+ # else
+ #   taken = true
+ # end
+ # taken
+
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+  # The below solution passed, but does not appear as a solution in git
+  # board[index] != " " && board[index] !=  "" && board[index] != nil
 end
