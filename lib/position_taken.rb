c5b7cf1bd57356fdca0cd132e431b9c5ffc400c6
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-07 22:54:23 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+
+  #new_index = index.to_i - 1
+  character = board[index]
+
+  if character == "X" || character == "O"
+    return true
+  else
+    return false
+  end
+
+end
