3186c261122b69a2c377faec1e42737ede413cc3
Arnold Su <arnoldtingsu@gmail.com>
2018-08-14 03:07:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+
+board = Array.new(9, ' ')
+
+def position_taken?(board, position)
+  if board[position] == " " || board[position] == nil || board[position] == ""
+    
+  return false
+
+  
+else 
+
+    return true
+  end
+end
+
