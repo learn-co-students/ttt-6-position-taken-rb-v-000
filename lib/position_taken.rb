5ac3c69d532f4bb91262c6d19f4d3b9a5ba196e5
Katie Kavett <katie.kavett@flatironschool.com>
2018-09-03 22:19:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(board,index)
+  if board[index] == (" ")
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || "O"
+    true
+  else
+    false
+  end
+end
