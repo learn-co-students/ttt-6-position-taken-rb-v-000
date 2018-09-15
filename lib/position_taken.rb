4ca244641c689d0767c6f82e9949b66734a80208
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-15 09:51:04 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+def position_taken?(board, x)
+  if board[x] == " "
+    return false 
+ return false
+ elsif board[x] == ""
+ return false
+  elsif board[x] == "X" || board[x] == "O"
+    return true 
+  else 
+    return false
+  end
+end
