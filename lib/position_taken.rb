aafcacc81f3229e9bb01c5676b50a41dbaf3276a
kitte anderson <kitteanderson@gmail.com>
2018-09-26 16:51:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 
   #another way to write position_taken?
-  def position_taken?(board, index=O)
-    if board[index] == " " || board[index] == "" || board[index] == nil
+  def position_taken?(board, index = 0)
+    if board[index] == " " || board[index] == "" || board[index]== nil
       false
     else
       true
-    end
   end
+end
