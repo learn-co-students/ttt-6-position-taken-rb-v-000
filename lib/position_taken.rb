0af5e2624e6ebba55a3863bf52596835e726943c
C-Lion Stepansky <esstepansky@gmail.com>
2018-12-27 00:11:17 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "x" || board[index] == "O" || board[index] == "o"
-    return true
-    elsif board[index] == " " || board[index] == "" || board[index] == NIL
+  if board[index] == " " || board[index] == "" || board[index] == NIL
     return false
+  else
+    return true
   end
 end
