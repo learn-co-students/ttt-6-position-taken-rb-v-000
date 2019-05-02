4aed7126766df6ddf7cf8e6032fee954bb5412af
Joel Croitoru <jcroitoru@gmail.com>
2019-05-02 18:38:19 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == "X"
+  if board[index] == "X" || board[index] == "O"
     true
-  elsif board[index] == "O"
-    true
-  elsif board[index] == " " || "" 
+  elsif board[index] == " " || ""
     false
   end
 end
