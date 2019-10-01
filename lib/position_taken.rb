b7811c10c35bf8b84d0f8b92c622e6686e9c263b
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-10-01 23:37:28 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
-board = ["  ","  ","  ", "  ","  ","  ","  ","  ","  "]
-
-def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
-    true
-  elsif board[index] != "  "  || board[index] != " " || board[index] != nil
-    false
-  end
-end
+def position_taken? (board, index)
+   if board[index] == " " || board[index] == "" || board[index] == nil
+     false
+   else
+     true
+     end
+ end
