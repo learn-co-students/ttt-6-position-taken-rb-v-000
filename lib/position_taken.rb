1df64546774cf9948b38a642ca1a6da09d5724ab
mstmari <michael.landrum.st.marie@gmail.com>
2019-10-29 12:38:58 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
 require 'pry'
 def position_taken?(board, index)
-  binding.pry
   (board[index] == " " || board[index] == "" || board[index] == nil) ?
   false : true
 end
