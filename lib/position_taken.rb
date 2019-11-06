2d075b1fd5a7c74664de445540fc8c1e464f3879
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-11-06 19:03:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   pos = board[index]
-  # if pos === " " || pos === "" || pos === nil
-  #   return false
-  # else
-  #   return true
-  # end
+
   return pos === "" || pos === " " || pos === nil ? false : true
-  
 end
