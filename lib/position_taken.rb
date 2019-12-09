8ade54faf5d6270dcb710bd0b8e822e75993b12f
valeria campilongo <cvaleriac@hotmail.it>
2019-12-09 19:27:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,24 +1,10 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
-def input_to_index (index)
- index = index.to_i
- index = index - 1
-end
-
-def move (board, index, value = "X")
-  board[index.to_i] = value
-end
 
 def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index] == nil)
-    return false
-  else #(board[index] == "X" || board[index]== "O")
-    return true
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    board[index] == "X" || board[index] == "O"
+    true
   end
 end
