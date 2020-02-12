0159ccc368986e2085b1091035b58932057a48ff
Guess <guess@Guesss-iMac.local>
2020-02-12 11:43:09 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-    return false
-  else
-    return true  #board[index] is not " " or "" or nil because there's an "X" or "O"
-  end 
+   (board[index] == " ") || (board[index] == "") || (board[index] == nil) ?
+    false : true
 end
