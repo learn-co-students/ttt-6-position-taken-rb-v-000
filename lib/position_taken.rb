7d3cc7c5d02fc01b177afd59d077e5c8a3ff0d77
Paul Eisele <paulheisele@gmail.com>
2020-02-29 16:11:00 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] != " "
-    return false
-  elsif board[index] == ""
+  elsif board[index] != " " || board[index] == ""
     return false
   else board[index] == nil
     return false
