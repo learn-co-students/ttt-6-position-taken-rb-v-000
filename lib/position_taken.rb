ef8c812a2415321533c640aed8ea3bb5110087c7
github username <github email address>
2020-05-20 14:30:22 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
+  
   if board[index] == " "
     return false
   elsif board[index] == ""
@@ -9,4 +10,5 @@ def position_taken?(board,index)
   else board[index] == "X" || "O"
     return true
   end
+
 end
