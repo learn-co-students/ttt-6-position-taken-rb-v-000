29263a3ace9e34b8b3fa0990a88c474d88921993
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-03 18:12:14 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == ' ' || board[index] == '' || board[index] == nil
-    return FALSE
+    return false
   end
   
   if board[index] == 'X' || board[index] == 'O'
-    return TRUE
+    return true
   end
 end
